457ba09095866adfa9d0b127a1d07e28fc069c51
InvalidJokerDE <0limitdev@gmail.com>
2024-05-26 18:53:36 +0200

    add

---
+++ src/routes/cards/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import DemoCookieSettings from './CookieSettings.svelte';
+  import DemoCreateAccount from './CreateAccount.svelte';
+  import DemoContainer from './DemoContainer.svelte';
+  import DemoNotifications from './Notifications.svelte';
+</script>
+
+<div class="items-start justify-center gap-6 p-8 rounded-lg md:grid lg:grid-cols-2 xl:grid-cols-3">
+  <div class="grid items-start col-span-3 gap-6 lg:col-span-1">
+    <DemoContainer>
+      <DemoCreateAccount />
+    </DemoContainer>
+    <DemoContainer>
+      <DemoNotifications />
+    </DemoContainer>
+  </div>
+  <div class="grid items-start col-span-3 gap-6 lg:col-span-2">
+    <DemoContainer>
+      <DemoCookieSettings />
+    </DemoContainer>
+  </div>
+</div>
---
+++ src/routes/cards/CreateAccount.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { Icons } from '$components/icons';
+  import { Button } from '$components/ui/button';
+  import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
+  } from '$components/ui/card';
+  import { Input } from '$components/ui/input';
+  import { Label } from '$components/ui/label';
+</script>
+
+<Card>
+  <CardHeader class="space-y-1">
+    <CardTitle class="text-2xl">Create an account</CardTitle>
+    <CardDescription>Enter your email below to create your account</CardDescription>
+  </CardHeader>
+  <CardContent class="grid gap-4">
+    <div class="relative">
+      <div class="absolute inset-0 flex items-center">
+        <span class="w-full border-t" />
+      </div>
+      <div class="relative flex justify-center text-xs uppercase">
+        <span class="px-2 bg-background text-muted-foreground"> Or continue with </span>
+      </div>
+    </div>
+    <div class="grid gap-2">
+      <Label for="email">Email</Label>
+      <Input id="email" type="email" placeholder="m@example.com" />
+    </div>
+    <div class="grid gap-2">
+      <Label for="password">Password</Label>
+      <Input id="password" type="password" />
+    </div>
+  </CardContent>
+  <CardFooter>
+    <Button class="w-full">Create account</Button>
+  </CardFooter>
+</Card>
