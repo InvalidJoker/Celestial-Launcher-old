4c46b3fee7cb2b5d29c2005706f14608b9940ea6
InvalidJokerDE <0limitdev@gmail.com>
2024-05-26 22:42:13 +0200

    add rust things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,2 @@
-<script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri";
-  import Background from "../images/bg.png";
-
-  let name = "";
-  let greetMsg = "";
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name });
-  }
-</script>
-
 <a href="/auth">Login</a>
 <a href="/pack">Pack</a>
